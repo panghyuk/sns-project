a50a7ac92fd5c550708ede9f481b4df04ef05d74
2017112570 <okh9712@gmail.com>
2021-04-22 22:19:22 +0900

    project setting, add main app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # sns-project
+20201 동국대학교 멋쟁이 사자처럼 9th sns project
---
+++ sns_project/main/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def showmain(request):
+    return render(request,'main/main.html')
